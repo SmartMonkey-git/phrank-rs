06cfad94baaa0d87bb5d790af1b75b9368968861
SmartMonkey <rouven.reuter91@gmail.com>
2026-04-22 18:04:20 +0200

    formatting

--- phrank-rs/src/error.rs
+++ phrank-rs/src/error.rs
@@ -1,11 +1,9 @@
 use thiserror::Error;
 
-
 #[derive(Error, Debug)]
-pub enum PhrankError{
+pub enum PhrankError {
     #[error("Could not load ontology: {0}")]
     ReadError(String),
     #[error("TermID not found: {0}")]
     TermIdNotFound(String),
 }
-
