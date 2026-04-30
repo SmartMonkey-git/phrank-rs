95011aa64d8c5031ba6471d0b696a99be4591f96
SmartMonkey <rouven.reuter91@gmail.com>
2026-04-30 10:26:29 +0200

    Remove unused error

--- phrank-rs/src/error.rs
+++ phrank-rs/src/error.rs
@@ -2,8 +2,6 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum PhrankError {
-    #[error("Could not load ontology: {0}.")]
-    ReadError(String),
     #[error("TermID not found: {0}.")]
     TermIdNotFound(String),
     #[error("Cohort needs to be >= 2. Got {0}.")]
