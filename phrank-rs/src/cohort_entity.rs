9e0f981679ddc1fbbe93b1d5530e6ecac2777fbe
SmartMonkey <rouven.reuter91@gmail.com>
2026-06-14 23:04:10 +0200

    String in favor of str

--- phrank-rs/src/cohort_entity.rs
+++ phrank-rs/src/cohort_entity.rs
@@ -1,15 +1,18 @@
 #[derive(Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub struct CohortEntity<'a> {
-    id: &'a str,
+    id: String,
     features: Vec<&'a str>,
 }
 
 impl<'a> CohortEntity<'a> {
-    pub fn new(id: &'a str, features: Vec<&'a str>) -> Self {
-        Self { id, features }
+    pub fn new(id: impl Into<String>, features: Vec<&'a str>) -> Self {
+        Self {
+            id: id.into(),
+            features,
+        }
     }
     pub fn id(&self) -> &str {
-        self.id
+        self.id.as_ref()
     }
     pub fn features(&self) -> &[&'a str] {
         &self.features
